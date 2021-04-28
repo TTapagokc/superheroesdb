4617a2aaabcc9d4d74e21b48f03078c59327a6b5
TTapagokc <ttapagokc1337@gmail.com>
2021-04-29 00:33:16 +0300

    style update

--- src/components/modelItem.jsx
+++ src/components/modelItem.jsx
@@ -1,4 +1,4 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import React from "react";
 import './modelItem.css';
 
@@ -6,26 +6,27 @@ const ModelItem = (props) => {
 
     return (
         <div className='modelitem-grid'>
-           <div className='grid-container'>
-           <NavLink to={"/modelsinfo/" + props.id} activeClassName='activeLink'>
-                <img className='modelImage' alt='avatar' src={props.heroImage}/>
-                {props.name}</NavLink>
-                <div>
-                    <span>{props.nickname}</span>
-                </div>
-                <div>
-                    <span>{props.real_name}</span>
-                </div>
-                <div>
-                    <span>{props.origin_description}</span>
-                </div>
-                <div>
-                    <span>{props.superpowers}</span>
-                </div>
-                <div>
-                    <span>{props.catch_phrase}</span>
-                </div>
-           </div>
+            <div className='grid-container'>
+                <NavLink to={"/modelsinfo/" + props.id} activeClassName='activeLink'>
+                    <img className='modelImage' alt='avatar' src={props.heroImage} />
+                    {props.name}
+                    <div>
+                        <span>{props.nickname}</span>
+                    </div>
+                    <div>
+                        <span>{props.real_name}</span>
+                    </div>
+                    <div>
+                        <span>{props.origin_description}</span>
+                    </div>
+                    <div>
+                        <span>{props.superpowers}</span>
+                    </div>
+                    <div>
+                        <span>{props.catch_phrase}</span>
+                    </div>
+                </NavLink>
+            </div>
         </div>
     );
 };
